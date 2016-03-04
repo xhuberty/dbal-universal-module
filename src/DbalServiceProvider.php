919f027deee270af873b3442acd01034469902b4
xhuberty <x.huberty@thecodingmachine.com>
2016-03-04 18:54:04 +0100

    Final version  before test

--- src/DbalServiceProvider.php
+++ src/DbalServiceProvider.php
@@ -10,6 +10,8 @@ namespace TheCodingMachine;
 
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Driver;
 use Interop\Container\ContainerInterface;
 use Interop\Container\ServiceProvider;
 
@@ -18,17 +20,76 @@ class DbalServiceProvider implements ServiceProvider
     public static function getServices()
     {
         return [
-            Connection::class => 'createConnection'
+            Connection::class => 'createConnection',
+            'dbal.host'=> 'getHost',
+            'dbal.user'=> 'getUser',
+            'dbal.password'=> 'getPassword',
+            'dbal.port'=> 'getPort',
+            'dbal.dbname'=> 'getDbname',
+            'dbal.charset'=> 'getCharset',
+            'dbal.driverOptions'=> 'getDriverOptions',
+            Driver::class => 'getDriver'
         ];
     }
     public static function createConnection(ContainerInterface $container, callable $previous = null) : Connection
     {
-        $params = $container->get(Connection::class.'.params');
-        $driver = $container->get(Connection::class.'.driver');
+        if($container->has(Connection::class.'.params')) {
+            $params = $container->get('dbal.params');
+        } else {
+            $params = array(
+                'host' => $container->get('dbal.host'),
+                'user' =>$container->get('dbal.user'),
+                'password' => $container->get('dbal.password'),
+                'port' => $container->get('dbal.port'),
+                'dbname' => $container->get('dbal.dbname'),
+                'charset' => $container->get('dbal.charset'),
+                'driverOptions' => $container->get('dbal.driverOptions')
+            );
+        }
+
+        $driver = $container->get(Driver::class);
 
         $connection =  new Connection($params, $driver);
 
         return $connection;
     }
 
+    public static function getHost() :string
+    {
+        return 'localhost';
+    }
+
+    public static function getUser():string
+    {
+        return 'root';
+    }
+
+    public static function getPassword():string
+    {
+        return '';
+    }
+
+    public static function getPort():int
+    {
+        return 3306;
+    }
+
+    public static function getDbname():string
+    {
+        throw new DBALException('The "dbname" must be set in the container entry "dbal.dbname"');
+    }
+
+    public static function getCharset():string
+    {
+        return 'utf8';
+    }
+
+    public static function getDriverOptions():array
+    {
+        return array(1002 =>"SET NAMES utf8");
+    }
+    public static function getDriver():Driver
+    {
+        return new Driver\PDOMySql\Driver();
+    }
 }
